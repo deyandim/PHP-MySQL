678a61a045583c957617a9daf59488c0909d58b6
Deyan Dimov <41387847+deyandim@users.noreply.github.com>
2019-05-27 01:03:54 +0300

    upload

--- ValidationForm/index.php
+++ ValidationForm/index.php
@@ -1,100 +1,113 @@
 <?php
-
-$error = '';
-if(isset($_POST['Submit'])){
-
+$nameError = '';
+$emailError = '';
+$genderError = '';
+$websiteError = '';
+$showData = true;
+if (isset($_POST['submit'])){
     if(empty($_POST['name'])){
-        $error = 'Required name';
+        $nameError = 'Name is required!';
+        $showData = false;
     } else {
-        $error = 'done';
+        $name = userInput($_POST['name']);
+        if (!preg_match('/^[A-Za-z \s]*$/m',$name)){
+            $nameError = 'Only letters and spaces are allowed';
+            $showData = false;
+        }
+    }
+    if(empty($_POST['email'])){
+        $emailError = 'Email is required!';
+        $showData = false;
+    } else {
+        $email = userInput($_POST['email']);
+        if (!preg_match("/^([a-z0-9\+_\-]+)(\.[a-z0-9\+_\-]+)*@([a-z0-9\-]+\.)+[a-z]{2,6}$/ix", $email)){
+            $emailError = 'Invalid e-mail!';
+            $showData = false;
+        }
+    }
+    if(empty($_POST['gender'])){
+        $genderError = 'Gender is required!';
+        $showData = false;
+    } else {
+        $gender = userInput($_POST['gender']);
+    }
 
+    $website = userInput($_POST['website']);
+    if (!preg_match( '#((https?|ftp)://(\S*?\.\S*?))([\s)\[\]{},;"\':<]|\.\s|$)#i',$website) && $website != ''){
+        $websiteError = 'Invalid website format!';
+        $website = '';
+    }
+
+    $comments = userInput($_POST['comment']);
+
+    if ($showData){
+        echo "<h2>Your submit Information</h2><br>";
+        echo "Name: " . ucwords($name) . "<br>";
+        echo "Email: {$email}<br>";
+        echo "Gender: {$gender}<br>";
+        echo "Website: {$website}<br>";
+        echo "Comments: {$comments}<br>";
+    } else {
+        echo "Please complete and correct your Form again!";
     }
 }
 
+//function isRequired($nameField){
+//    if (empty($nameField)){
+//        $errorString = 'This field is required!';
+//        return $errorString;
+//    } else {
+//        return userInput($nameField);
+//    }
+//}
 
-function userInputValidate($input){
-
+function userInput($data){
+    return $data;
 }
+
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <title>Form Validation</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+          integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
 </head>
 <body>
-<div class="container">
-    <div class="row">
-        <div class="col-7">
-            <!-- Default form contact -->
-            <form class="text-center border border-light p-5" method="post">
 
-                <p class="h4 mb-4">Contact us</p>
-
-                <!-- Name -->
-                <div class="row">
-                    <div class="col-10">
-                        <input type="text" id="defaultContactFormName" class="form-control mb-4" name="name" placeholder="Name">
-
-                    </div>
-                    <div class="col-2 pl-0" style="color: red">*<?= $error ?></div>
-                </div>
-
-                <!-- Email -->
-                <div class="row">
-                    <div class="col-10">
-                        <input type="email" id="defaultContactFormEmail" class="form-control mb-4" name="email" placeholder="E-mail">
-                    </div>
-                    <div class="col-1 pl-0" style="color: red">*</div>
-                </div>
-
-                <!-- Group of default radios - Female -->
-               <div class="container">
-                   <div class="row">
-                       <div class="custom-control custom-radio col-5">
-                           <input type="radio" class="custom-control-input" id="defaultGroupExample1" name="gender">
-                           <label class="custom-control-label" for="defaultGroupExample1">Female</label>
-                       </div>
-
-                       <!-- Group of default radios - Male -->
-                       <div class="custom-control custom-radio col-5">
-                           <input type="radio" class="custom-control-input" id="defaultGroupExample2" name="gender">
-                           <label class="custom-control-label" for="defaultGroupExample2">Male</label>
-                       </div>
-                       <div class="col-1" style="color: red">*</div>
-                   </div>
-               </div><br>
-                <!-- Website -->
-                <div class="row">
-                    <div class="col-10">
-                        <input type="text" id="defaultContactFormWebsite" class="form-control mb-4" name="website" placeholder="Website">
-                    </div>
-                    <div class="col-1 pl-0" style="color: red">*</div>
-                </div>
-
-                <!-- Message -->
-                <div class="row">
-                    <div class="form-group col-10">
-                        <textarea class="form-control rounded-0" id="exampleFormControlTextarea2" rows="3" placeholder="Message"></textarea>
-                    </div>
-                    <div class="col-1 pl-0" style="color: red">*</div>
-                </div>
-
-
-                <!-- Copy -->
-                <div class="custom-control custom-checkbox mb-4">
-                    <input type="checkbox" class="custom-control-input" id="defaultContactFormCopy">
-                    <label class="custom-control-label" for="defaultContactFormCopy">Send me a copy of this message</label>
-                </div>
-
-                <!-- Send button -->
-                <button class="btn btn-info btn-block" type="submit" name="Submit">Send</button>
-
-            </form>
-            <!-- Default form contact -->
-        </div>
+<h2>Form Validation with PHP</h2>
+<div class="row container">
+    <div class="col-6">
+        <form method="post" style="padding: 5px">
+            <fieldset>
+                Name:<br>
+                <input type="text" name="name">
+                <span style="color: red">* <?= $nameError ?></span><br>
+                <br>
+                E-mail:<br>
+                <input type="text" name="email">
+                <span style="color: red">* <?= $emailError ?></span><br>
+                <br>
+                Gender:<br>
+                <input type="radio" name="gender" value="male">Male
+                <input type="radio" name="gender" value="female">Female
+                <span style="color: red">* <?= $genderError ?></span>
+                <br>
+                <br>
+                Website:<br>
+                <input name="website"><span style="color: red"> <?= $websiteError ?></span><br>
+                <br>
+                Comments:<br>
+                <textarea name="comment" rows="5" cols="25"></textarea>
+                <br>
+                <br>
+                <input type="submit" name="submit">
+            </fieldset>
+        </form>
     </div>
 </div>
 
